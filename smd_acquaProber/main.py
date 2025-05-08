06c656a9de56e107bf693170226d28d8d318bead
fede-chiodi <federicochiodi07@gmail.com>
2025-05-08 10:42:41 +0200

    feeder implementation

--- smd_acquaProber/main.py
+++ smd_acquaProber/main.py
@@ -12,6 +12,7 @@ from sensors.ph_sensor import read_ph
 from sensors.turbidity_sensor import read_turbidity
 from sensors.temperature_sensor import read_temperature
 from sensors.conductivity_sensor import read_conductivity
+from sensors.feeder_servo import open_valve, close_valve, is_valve_open, cleanup
 
 
 class SensorWindow(QWidget):
@@ -61,6 +62,57 @@ class SensorWindow(QWidget):
         self.plot_widget.setTitle(f"<span style='color:white;'>{self.windowTitle()}</span>")
         self.label.setText(f"Ultimo valore: <b>{value:.2f} {self.unit}</b>")
 
+from sensors.feeder_servo import open_valve, close_valve, is_valve_open
+
+class FeederWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Controllo Feeder")
+        self.resize(400, 300)
+        self.setStyleSheet("background-color: #121212; color: white;")
+
+        layout = QVBoxLayout()
+        layout.setAlignment(Qt.AlignCenter)
+
+        self.status_label = QLabel()
+        self.status_label.setFont(QFont("Arial", 16))
+        self.status_label.setAlignment(Qt.AlignCenter)
+        layout.addWidget(self.status_label)
+
+        self.control_button = QPushButton("Apri Valvola")
+        self.control_button.setFont(QFont("Arial", 14))
+        self.control_button.setFixedSize(200, 50)
+        self.control_button.setStyleSheet("""
+            QPushButton {
+                background-color: #007ACC;
+                color: white;
+                border-radius: 10px;
+            }
+            QPushButton:hover {
+                background-color: #005F99;
+            }
+        """)
+        self.control_button.clicked.connect(self.toggle_valve)
+        layout.addWidget(self.control_button, alignment=Qt.AlignCenter)
+
+        self.setLayout(layout)
+        self.update_status()
+
+    def update_status(self):
+        if is_valve_open():
+            self.status_label.setText("Stato: <b style='color: lime;'>Aperta</b>")
+            self.control_button.setText("Chiudi Valvola")
+        else:
+            self.status_label.setText("Stato: <b style='color: red;'>Chiusa</b>")
+            self.control_button.setText("Apri Valvola")
+
+    def toggle_valve(self):
+        if is_valve_open():
+            close_valve()
+        else:
+            open_valve()
+        self.update_status()
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -104,6 +156,26 @@ class MainWindow(QMainWindow):
 
         layout.addLayout(button_layout)
 
+        feeder_button = QPushButton()
+        feeder_button.setIcon(QIcon("icons/feeder.jpg"))
+        feeder_button.setIconSize(QSize(100, 100))
+        feeder_button.setFixedSize(130, 130)
+        feeder_button.setStyleSheet("""
+            QPushButton {
+                background-color: #2e2e2e;
+                border-radius: 15px;
+                border: 2px solid #444;
+            }
+            QPushButton:hover {
+                background-color: #444;
+                border: 2px solid cyan;
+            }
+        """)
+        feeder_button.setToolTip("Feeder")
+        feeder_button.clicked.connect(self.open_feeder_window)
+        button_layout.addWidget(feeder_button)
+
+
         exit_button = QPushButton("Esci")
         exit_button.setFixedSize(150, 50)
         exit_button.setFont(QFont("Arial", 14))
@@ -127,10 +199,17 @@ class MainWindow(QMainWindow):
     def open_sensor_window(self, name, func, unit):
         self.sensor_window = SensorWindow(name, func, unit)
         self.sensor_window.show()
+    
+    def open_feeder_window(self):
+        self.feeder_window = FeederWindow()
+        self.feeder_window.show()
 
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    sys.exit(app.exec_())
+    try:
+        app = QApplication(sys.argv)
+        window = MainWindow()
+        window.show()
+        sys.exit(app.exec_())
+    finally:
+        cleanup()
